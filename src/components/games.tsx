2d21396693873697fb4e674c9e542eec04867ae3
Ethan Davies <application77863@gmail.com>
2023-09-25 19:15:33 +0100

    Added Games section

--- src/components/games.tsx
+++ src/components/games.tsx
@@ -1,20 +1,40 @@
 import '../css/games.css'
 import type { Game } from '../data/games'
 import { games } from '../data/games'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLockOpen, faLock, IconDefinition } from '@fortawesome/free-solid-svg-icons'
+import { favGame } from '../data/games'
 
 function Games() {
+
+    let svg: IconDefinition;
+    
+    if(favGame.locked) {
+        svg = faLock
+    } else {
+        svg = faLockOpen
+    }
+
     return (
         <>
-            
-            <div id="games">
-            {games.map((props: Game) => {
-                return (
-
-                    <div className='game__item'>
-                        <img src={props.image} alt={props.name} />
+            <div id="games-container">
+                <div id="games">
+                    <div className='game__icon'>
+                        <img src={favGame.image} alt={favGame.name} />
                     </div>
-                )
-            })}
+                {games.map((props: Game) => {
+                    return (
+
+                        <div className='game__item'>
+                            <img src={props.image} alt={props.name} />
+                        </div>
+                    )
+                })}
+            </div>
+            <div id="favGame-container">
+                <span>{favGame.name}</span><div className='vl'></div><span className='console'>{favGame.console}</span>
+                <FontAwesomeIcon className="icon fa-sm" icon={svg}></FontAwesomeIcon>
+            </div>
             </div>
         </>
     )
--- src/data/games.ts
+++ src/data/games.ts
@@ -4,17 +4,59 @@ export interface Game {
     description?: string,
 }
 
+export interface favouriteGame {
+    name: string,
+    image: string,
+    locked: boolean,
+    console: string
+}
+
+export let favGame: favouriteGame = {
+    name: "NBA 2K",
+    image: "/src/assets/icons/NBA2k.png",
+    locked: true,
+    console: "PS5"
+}
+
 export const games: Game[] = [
     {
         name: "Minecraft",
-        image: "../assets/icons/mc2.png",
+        image: "/src/assets/icons/Minecraft.jpg",
     },
     {
-        name: "Minecraft",
-        image: "../assets/icons/mc2.png",
+        name: "CS2",
+        image: "/src/assets/icons/CS2.jpg",
     },
     {
-        name: "Minecraft",
-        image: "../assets/icons/mc2.png",
+        name: "Rainbow Six Siege",
+        image: "/src/assets/icons/R6.jpg",
+    },
+    {
+        name: "PlayStation+",
+        image: "/src/assets/icons/PlayStationPlus.png",
+    },
+    {
+        name: "Star Craft 2",
+        image: "/src/assets/icons/StarCraft2.jpg",
+    },
+    {
+        name: "Forza Horizon 5",
+        image: "/src/assets/icons/ForzaHorizon5.jpg",
+    },
+        {
+        name: "Fortnite",
+        image: "/src/assets/icons/Fortnite.jpg",
+    },
+    {
+        name: "Spiderman Remastered",
+        image: "/src/assets/icons/SpidermanRemastered.jpg",
+    },
+    {
+        name: "Terraria",
+        image: "/src/assets/icons/Terraria.jpg",
+    },
+    {
+        name: "Last of Us",
+        image: "/src/assets/icons/LastOfUs.png",
     },
 ]
