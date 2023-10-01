71b65a4f5288136e48c514b1311f3bcc957632ad
Ethan Davies <application77863@gmail.com>
2023-10-01 20:22:01 +0100

    Improved UI

--- src/components/games.tsx
+++ src/components/games.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react'
 import '../css/games.css'
 import type { Game } from '../data/games'
 import { games } from '../data/games'
@@ -6,17 +7,87 @@ import {
     faLockOpen,
     faLock,
     IconDefinition,
+    faXmark,
 } from '@fortawesome/free-solid-svg-icons'
 import { favGame } from '../data/games'
 import { motion } from 'framer-motion'
 
 function Games() {
     let svg: IconDefinition
+    const [gameName, setGameName] = useState('')
+    const [gameDescription, setGameDescription] = useState('')
+    const [gameLink, setGameLink] = useState('')
+    const [launchButtonText, setLaunchButtonText] = useState('Launch Game')
+    const [dots, setDots] = useState(0)
+    const [launching, setLaunching] = useState(false)
+    const [steamAvailable, setSteamAvailable] = useState(false)
 
     if (favGame.locked) {
         svg = faLock
     } else {
         svg = faLockOpen
+    }
+
+    const [modal, setModal] = useState(false)
+
+    const toggleModal = () => {
+        setModal(!modal)
+    }
+
+    const launchGame = () => {
+        if (launching) {
+            return // Don't do anything if launching is in progress
+        }
+
+        setLaunchButtonText('Launching Game')
+        setDots(0)
+        setLaunching(true)
+
+        const interval = setInterval(() => {
+            setDots((prevDots) => (prevDots % 3) + 1)
+        }, 1000)
+
+        setTimeout(() => {
+            clearInterval(interval)
+            setLaunchButtonText('Launch Game')
+            setDots(0)
+            setLaunching(false)
+        }, 10000) // Increase the duration to 10 seconds
+    }
+
+    const setModalDetails = async (
+        name: string,
+        description: string,
+        link: string,
+        steam: boolean
+    ): Promise<void> => {
+        setGameName(name)
+        setGameDescription(description)
+        setGameLink(link)
+        setSteamAvailable(steam) // Set Steam availability state
+        toggleModal()
+    }
+
+    useEffect(() => {
+        if (launching) {
+            // Disable the button during the "Launching Game..." animation
+            const launchButton = document.getElementById('launch-button')
+            if (launchButton) {
+                launchButton.classList.add('disabled')
+            }
+        } else {
+            // Re-enable the button when the animation is done
+            const launchButton = document.getElementById('launch-button')
+            if (launchButton) {
+                launchButton.classList.remove('disabled')
+            }
+        }
+    }, [launching])
+
+    if (modal) {
+        document.body.classList.add('active-modal')
+    } else {
+        document.body.classList.remove('active-modal')
     }
 
     return (
@@ -41,9 +112,23 @@ function Games() {
                                 viewport={{ once: true }}
                                 className="game__item"
                             >
-                                <a href={props.link}>
+                                <a
+                                    onClick={() =>
+                                        setModalDetails(
+                                            props.name,
+                                            props.description,
+                                            props.link,
+                                            props.steam
+                                        )
+                                    }
+                                >
                                     <img src={props.image} alt={props.name} />
                                 </a>
+                                {!props.steam && (
+                                    <div className="steam-disabled">
+                                        Not available on Steam
+                                    </div>
+                                )}
                             </motion.div>
                         )
                     })}
@@ -102,6 +187,89 @@ function Games() {
                     </motion.div>
                 </div>
             </div>
+
+            {/* Pop-up */}
+            {modal && (
+                <motion.div
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{
+                        type: 'spring',
+                        bounce: 0,
+                        duration: 0.5,
+                        delay: 0.1,
+                    }}
+                    className="modal"
+                >
+                    <div onClick={toggleModal} className="overlay"></div>
+                    <div className="modal-content">
+                        <motion.h2
+                            initial={{ opacity: 0, y: 20 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            transition={{
+                                type: 'spring',
+                                bounce: 0,
+                                duration: 0.5,
+                                delay: 0.2,
+                            }}
+                            className="modal-title"
+                        >
+                            {gameName}
+                        </motion.h2>
+                        <motion.p
+                            initial={{ opacity: 0, y: 20 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            transition={{
+                                type: 'spring',
+                                bounce: 0,
+                                duration: 0.5,
+                                delay: 0.35,
+                            }}
+                            className="modal-paragraph"
+                        >
+                            {gameDescription}
+                        </motion.p>
+                        <motion.div
+                            initial={{ opacity: 0, y: 20 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            transition={{
+                                type: 'spring',
+                                bounce: 0,
+                                duration: 0.5,
+                                delay: 0.6,
+                            }}
+                        >
+                            <FontAwesomeIcon
+                                onClick={toggleModal}
+                                icon={faXmark}
+                                className="fa-lg close-modal"
+                            ></FontAwesomeIcon>
+                        </motion.div>
+                        {steamAvailable && (
+                            <motion.a
+                                id="launch-button"
+                                onClick={launchGame}
+                                href={gameLink}
+                                className={`launch-button ${
+                                    launching ? 'disabled' : ''
+                                }`}
+                                initial={{ opacity: 0, y: 20 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{
+                                    type: 'spring',
+                                    bounce: 0,
+                                    duration: 0.5,
+                                    delay: 0.6,
+                                }}
+                            >
+                                {launching
+                                    ? `Launching Game${'.'.repeat(dots)}`
+                                    : 'Launch Game'}
+                            </motion.a>
+                        )}
+                    </div>
+                </motion.div>
+            )}
         </>
     )
 }
--- src/data/games.ts
+++ src/data/games.ts
@@ -1,7 +1,10 @@
 export interface Game {
     name: string
     image: string
-    link: string
+    link: string,
+    steam: boolean
+
+    description: string
 }
 
 export interface favouriteGame {
@@ -27,50 +30,80 @@ export const games: Game[] = [
         name: 'Minecraft',
         image: '/src/assets/icons/Minecraft.jpg',
         link: '',
+        description:
+            "Minecraft, developed by Mojang Studios, is a groundbreaking sandbox game that invites players to embark on limitless adventures in procedurally generated worlds made of blocks. This open-ended, creative playground allows you to build, explore, mine, and survive as you craft your own unique experiences. Whether you're constructing grand structures, battling mobs, or embarking on epic quests, Minecraft offers endless possibilities.",
+        steam: false
     },
     {
-        name: 'CS2',
+        name: 'Counter-Strike 2',
         image: '/src/assets/icons/CS2.jpg',
         link: 'steam://run/730',
+        description:
+            "CS2, developed by Valve, is the eagerly awaited successor to the iconic first-person shooter, Counter-Strike: Global Offensive (CS:GO). CS2 retains the heart-pounding intensity and team-based gameplay that fans love. Players once again have the choice to join either the terrorists or counter-terrorists in a variety of game modes. This highly anticipated release promises updated graphics, mechanics, and potentially new features, building upon the franchise's rich history of competitive gaming. Stay tuned for the latest updates on this thrilling addition to the Counter-Strike series.",
+        steam: true
     },
     {
         name: 'Rainbow Six Siege',
         image: '/src/assets/icons/R6.jpg',
         link: 'steam://run/359550',
+        description:
+            'Rainbow Six Siege is an immersive tactical shooter developed by Ubisoft. It challenges players to become elite special forces operators, mastering the art of strategy and teamwork in high-stakes, close-quarters combat scenarios. With a diverse roster of operators and ever-evolving gameplay, every match offers a dynamic and intense experience.',
+        steam: true
     },
     {
         name: 'PlayStation+',
         image: '/src/assets/icons/PlayStationPlus.png',
         link: '',
+        description:
+            'PlayStation+ is a video game subscription service developed and published by Sony Interactive Entertainment.',
+        steam: false
     },
     {
         name: 'Star Craft 2',
         image: '/src/assets/icons/StarCraft2.jpg',
         link: '',
+        description:
+            'Starcraft 2, the epic real-time strategy (RTS) game developed by Blizzard Entertainment, transports players to a rich sci-fi universe where they command powerful armies and vie for control of the galaxy. With three unique factions, engrossing single-player campaigns, and a thriving esports scene, Starcraft 2 is a hallmark of strategic warfare.',
+        steam: false
     },
     {
         name: 'Forza Horizon 5',
         image: '/src/assets/icons/ForzaHorizon5.jpg',
         link: 'steam://run/1551360',
+        description:
+            'Forza Horizon 5, set in the breathtaking landscapes of Mexico, redefines the racing genre with its open-world approach and stunning visual fidelity. Players can choose from an extensive collection of meticulously detailed cars and engage in thrilling, dynamic races across diverse terrains. The Horizon Festival awaits, promising unforgettable moments and endless driving adventures.',
+        steam: true
     },
     {
         name: 'Fortnite',
         image: '/src/assets/icons/Fortnite.jpg',
         link: '',
+        description:
+            'Fortnite, the iconic battle royale sensation developed by Epic Games, catapulted gaming into the mainstream with its unique blend of fast-paced action, building mechanics, and vibrant, ever-evolving world. Players dive into a dynamic, last-person-standing battle where creativity and skill determine victory. Constant updates and engaging events keep the Fortnite experience fresh and exciting.',
+        steam: false
     },
     {
         name: 'Spiderman Remastered',
         image: '/src/assets/icons/SpidermanRemastered.jpg',
         link: 'steam://run/1817070',
+        description:
+            'Spider-Man Remastered brings the beloved web-slinger to life in a stunningly reimagined New York City. Players step into the iconic suit of Spider-Man and swing through skyscrapers, combat nefarious villains, and unravel a gripping narrative. This remastered edition enhances the visuals and gameplay, making it a must-play for fans and newcomers alike.',
+        steam: true
     },
     {
         name: 'Terraria',
         image: '/src/assets/icons/Terraria.jpg',
         link: 'steam://run/105600',
+        description:
+            "Terraria, an indie gem developed by Re-Logic, offers a captivating blend of exploration, crafting, and adventure. In a pixel-art world filled with wonders and challenges, players mine resources, craft weapons, and face off against formidable bosses. The game's endless creativity and surprises await those who dare to dig deep.",
+        steam: true
     },
     {
         name: 'Last of Us',
         image: '/src/assets/icons/LastOfUs.png',
         link: 'steam://run/1888930',
+        description:
+            "The Last of Us, a masterpiece from Naughty Dog, plunges players into a post-apocalyptic world where survival comes at a steep cost. You'll follow the harrowing journey of Ellie and Joel as they navigate a world ravaged by a deadly infection and ruthless human factions. With its gripping narrative and emotional depth, this action-adventure epic is a true work of art.",
+        steam: true
     },
 ]
